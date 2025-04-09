0077af132cb3890424e5cf38ac4c4124e8357d37
DanielMedina003 <162385548+DanielMedina003@users.noreply.github.com>
2025-04-09 12:25:14 -0500

    EditOrder

--- movewise/hooks/api/apiClient.tsx
+++ movewise/hooks/api/apiClient.tsx
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from "axios";
 export const  url = 'http://192.168.18.164:8000/'
-export const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwZXJzb25faWQiOjEsImVtYWlsIjoiZXhhbXBsZUBleGFtcGxlLmNvbSIsImV4cCI6MTc0NDIyNTE4NCwiaWF0IjoxNzQ0MTM4Nzg0fQ.ClqZ2TwSBhNgsR6uhUFRCdn-OpMQrhJDOK1qpI6U23o"
+export const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwZXJzb25faWQiOjIsImVtYWlsIjoiZXhhbXBsZUBleGFtcGxlLmNvbSIsImV4cCI6MTc0NDMwNTc0MiwiaWF0IjoxNzQ0MjE5MzQyfQ.duutc-pua4R9l8YKYdZmZoUtgQgSD2BJmQ8YJS9nX4c"
 const apiClient: AxiosInstance = axios.create({
   baseURL: url,
   timeout: 10000, // 10 segundos de timeout
