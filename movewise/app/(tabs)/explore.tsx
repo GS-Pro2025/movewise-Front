d38f06258977f53d65a10053991abdb7d58cbed3
StivenColorado <stivenchoo@gmail.com>
2025-05-19 13:59:45 -0500

    upgrade to SDK 53

---
+++ movewise/app/(tabs)/explore.tsx
@@ -0,0 +1,10 @@
+// Ejemplo para app/(tabs)/explore.tsx
+import { View, Text } from 'react-native';
+
+export default function ExploreScreen() {
+  return (
+    <View>
+      <Text>Explore Screen</Text>
+    </View>
+  );
+}
